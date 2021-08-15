ad33fc23c7ff95f10a3eeb2fde8b97ef7890242f
RClarke318 <ryan.clarke5@mohawkcollege.ca>
2021-08-14 22:24:10 -0400

    Added a "Return" button to the "Game Over" screen

--- src/scenes/gameOver.js
+++ src/scenes/gameOver.js
@@ -1,7 +1,20 @@
+let gameOverBtnImg;
+let gameOverBtn;
+
 const gameOver = {
     "setup": function()
     {
-
+		gameOverBtnImg = loadImage("assets/textures/ReturnButton.png",
+			() => {
+				gameOverBtnImg = scaleNearestNeighbor(gameOverBtnImg, 301, 103);
+				gameOverBtn = new ImageButton(gameOverBtnImg, 13, 359,
+					() => {
+						currentScene = "Main Menu";
+						gameOverBtn.destroy();
+						scenes[currentScene]["setup"]();
+					});
+				gameOverBtn.init();
+			});
     },
     "draw": function()
     {
