79315f6742ceffc70d5af0b2a134afc0f557b6da
RClarke318 <ryan.clarke5@mohawkcollege.ca>
2021-08-20 20:48:03 -0400

    Updated gameOver.js

--- src/scenes/gameOver.js
+++ src/scenes/gameOver.js
@@ -1,15 +1,36 @@
 import Scene from "../objects/scene.js";
+import { scaleNearestNeighbor } from "../helperFunctions/imageFunctions.js";
+import ImageButton from "../objects/imageButton.js";
+import sceneManager from "../objects/sceneManager.js";
+import { MAIN_MENU } from "../constants/sceneConstants.js";
+import canvasManager from "../objects/canvasManager.js";
 
-/**
- * This scene will manage our Game Over screen.
- */
 export default class GameOver extends Scene {
+	
     constructor() {
         super();
-    }
+		this.gameOverBtn = {};
+		this.gameOverBtnImg = {};    
+	}
 
-    setup() {}
+    setup() {
+		this.gameOverBtnImg = loadImage("assets/textures/ReturnButton.png",
+			() => {
+				this.gameOverBtnImg = scaleNearestNeighbor(this.gameOverBtnImg, 301, 103);
+				this.gameOverBtn = new ImageButton(this.gameOverBtnImg, 13, 359,
+					() => { 
+						canvasManager.canvas.clear();
+						sceneManager.setCurrentScene(MAIN_MENU);
+					}
+				);
+			});
+			canvasManager.canvas.mouseClicked((event) => {
+			this.gameOverBtn.callback(event); 
+		});
+	}
+	
     draw() {
         background(0, 255, 255);
+		image(this.gameOverBtnImg, 13, 359);
     }
 }
