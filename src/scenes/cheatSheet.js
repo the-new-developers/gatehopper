c020ad34bd078d8c6419f6f9c951e6ec5fe14b14
Victor Sohier <1sohiervic@gmail.com>
2021-08-22 17:05:04 -0400

    reverted incorrect change to cheatsheet

--- src/scenes/cheatSheet.js
+++ src/scenes/cheatSheet.js
@@ -1,49 +1,42 @@
 import Scene from "../objects/scene.js";
-import { GateFactory, GateTypeEnum } from "../objects/gateFactory.js";
+import { scaleNearestNeighbor } from "../helperFunctions/imageFunctions.js";
+import ImageButton from "../objects/imageButton.js";
+import sceneManager from "../objects/sceneManager.js";
+import { MAIN_MENU } from "../constants/sceneConstants.js";
+import canvasManager from "../objects/canvasManager.js";
 
-/**
- * This scene will manage the game's challenge mode.
- */
-export default class ChallengeMode extends Scene {
-    constructor() {
-        super();
-        this.gates = [];
-    }
+export default class CheatSheet extends Scene {
+	constructor() {
+		super();
+		this.cheatSheetImg = {};
+		this.cheatSheetBtnImg = {};
+		this.cheatSheetBtn = {};
+	}
 
-    setup() {
-    	this.gateFactory = new GateFactory();
-    	Promise
-    		.all(this.gateFactory.preload())
-    		.then(() =>
-    		{
-    			//This callback will be moved to setup once the preloader is added
-    			this.constant1 = this.gateFactory.create(GateTypeEnum.CONST, 0);
-    			this.constant2 = this.gateFactory.create(GateTypeEnum.CONST, 1);
-
-				this.gates.push(
-					this.gateFactory.create(
-						1, this.constant1
-					)
+	setup() {
+		this.cheatSheetImg = loadImage("assets/textures/CheatSheet.png",
+			() => this.cheatSheetImg = scaleNearestNeighbor(this.cheatSheetImg, 640, 480));
+		this.cheatSheetBtnImg = loadImage("assets/textures/CheatSheetBackButton.png",
+			() => {
+				this.cheatSheetBtnImg = scaleNearestNeighbor(this.cheatSheetBtnImg, 301, 103);
+				this.cheatSheetBtn = new ImageButton(
+					this.cheatSheetBtnImg, 
+					13, 
+					359,
+					() => { 
+						canvasManager.canvas.clear();
+						sceneManager.setCurrentScene(MAIN_MENU);
+					}
 				);
+			});
 
-    			for (let i = 2; i < 8; i++)
-    			{
-    				this.gates.push(
-    					this.gateFactory
-    						.create(i,
-    							this.constant1,
-    							this.constant2)
-    				);
-    			}
-    		})
-    		.finally();
-    }
-    
-    draw() {
-        background(0, 0, 255);
-        this.constant1.show(100, 100);
-        this.constant2.show(100, 200);
-		for (let i = 0; i < this.gates.length; i++)
-        	this.gates[i].show(150, 150 + i * 50);
-    }
+			canvasManager.canvas.mouseClicked((event) => {
+			this.cheatSheetBtn.callback(event); 
+		});
+	}
+
+	draw() {
+		image(this.cheatSheetImg, 0, 0);
+		image(this.cheatSheetBtnImg, 13, 359);
+	}
 }
