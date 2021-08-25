d82d41be5431340cb232866c906ef565ac558763
Andy J <86134550+AndyJPL@users.noreply.github.com>
2021-08-25 10:45:21 -0400

    Update lifeBar.js

--- src/objects/lifeBar.js
+++ src/objects/lifeBar.js
@@ -17,6 +17,7 @@
         this.imgWidth = 180;//width of background bar image
         this.imgHeight = 35;//height of background bar image
         this.img = loadImage("assets/textures/challengeMode/lifeBar.png");//load image from assests
+        this.health = 100;//represented by width of dynamic rectangle, shows how much 'health' player has left
     }
   
     /**
@@ -30,31 +31,33 @@
   
     /**
      * decreases width of dynamic rectangle; depending on functionality of game, this function could be changed
-     * to accept an argument of how much 'life' is taken away, based on game action
+     * to accept an argument of how much health is taken away, based on game action
      */
     decreaseLife(){
       if(this.width > 0){
       this.width -= 6;
+      this.health -= 100/28;
       }
     }
   
     /**
      * increases width of dynamic rectangle; this function may be unnecessary, depending on whether player can
-     * get 'bonus life', to replenish 'life level'; depending on functionality of game, this function could be changed
-     * to include a parameter of how much 'life' is added, based on game action
+     * get 'bonus health', to replenish 'health'; depending on functionality of game, this function could be changed
+     * to include a parameter of how much health is added, based on game action
      */
     increaseLife(){
       if(this.width < 168){
       this.width += 6;
+      this.health += 100/28;
       }
     }
   
     /**
-     * used to return 'life level'; once this reaches zero, game is over
+     * used to return health; once this reaches zero, game is over
      * 
-     * @returns width of dynamically changing rectangle representing 'life level'
+     * @returns width of dynamically changing rectangle representing 'health'
      */
-    getLifeLevel(){
-      return this.width;
+    getHealth(){
+      return Math.abs(Math.trunc(this.health));
     }
   }
