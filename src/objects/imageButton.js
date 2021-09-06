111bdb4e502bb0598c36c3f521fedd394298bf1b
Victor Sohier <1sohiervic@gmail.com>
2021-09-06 10:27:57 -0400

    Changed buttons to remove callback hell, added hover callback

--- src/objects/imageButton.js
+++ src/objects/imageButton.js
@@ -1,15 +1,21 @@
 // TODO: Add some documentation here.
 export default class ImageButton {
 	
-	constructor(img, x, y, callback)
+	constructor(img, x, y)
 	{
 		this.img = img;
 		this.x = x;
 		this.y = y;
-		this.callback = function (event) {
+		this.clickCallback = function (event) {
 			if (mouseX > x && mouseX < x + img.width && mouseY > y && mouseY < y + img.height)
 			{
-				callback(event);
+			
+			}
+		};
+		this.hoverCallback = function (event) {
+			if (mouseX > x && mouseX < x + img.width && mouseY > y && mouseY < y + img.height)
+			{
+			
 			}
 		};
 	}
@@ -17,5 +23,37 @@ export default class ImageButton {
 	show()
 	{
 		image(this.img, this.x, this.y);
+	}
+
+	/// <summary>
+	/// Adds an action to the click action of the button
+	/// </summary>
+	/// <param name="callback">The action the button takes upon being clicked</param>
+	/// <returns>"this" for chaining</returns>
+	click(callback)
+	{
+		this.clickCallback = function (event) {
+			if (mouseX > x && mouseX < x + img.width && mouseY > y && mouseY < y + img.height)
+			{
+				callback(event);
+			}
+		};
+		return this;
+	}
+
+	/// <summary>
+	/// Adds an action to the hover action of the button
+	/// </summary>
+	/// <param name="callback">The action the button takes upon being hovered over</param>
+	/// <returns>"this" for chaining</returns>
+	hover(callback)
+	{
+		this.hoverCallback = function (event) {
+			if (mouseX > x && mouseX < x + img.width && mouseY > y && mouseY < y + img.height)
+			{
+				callback(event);
+			}
+		};
+		return this;
 	}
 }
